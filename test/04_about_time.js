2639b4e5e1b1072d6fbd0b33d1b73c4ab7df35d6
Jo Van Eyck <jo.vaneyck@ae.be>
2018-05-15 15:34:13 +0200

    04 solutions

--- test/04_about_time.js
+++ test/04_about_time.js
@@ -121,7 +121,7 @@ asyncTest('buffering', function () {
     events.onNext('s');
 
     setTimeout(function () {
-      equal(__, received.join(' '));
+      equal('RxJS Rocks', received.join(' '));
       start();
     }, 120);
   }, 120);
@@ -132,7 +132,7 @@ asyncTest('time between calls', function () {
   var events = new Subject();
 
   events.timeInterval()
-    .filter(function (t) { return t.interval > 100; })
+    .filter(function (t) { return t.interval > 5; })
     .subscribe(function (t) { received.push(t.value); });
 
   events.onNext('too');
@@ -144,21 +144,22 @@ asyncTest('time between calls', function () {
     setTimeout(function () {
       events.onNext('down');
 
-      equal(__, received.join(' '));
+      equal('slow down', received.join(' '));
       start();
-    }, 120);
-  }, 120);
+    }, 10);
+  }, 10);
 });
 
 asyncTest('results can be ambiguous timing', function () {
   var results = 0;
-  var fst = Observable.timer(400).map(-1);
-  var snd = Observable.timer(500).map(1);
+  var fst = Observable.timer(4).map(-1);
+  var snd = Observable.timer(5).map(1);
 
+  //amb: Propagates the observable sequence or Promise that reacts first. "amb" stands for ambiguous.
   fst.amb(snd).subscribe(function (x) { results = x; });
 
   setTimeout(function () {
-    equal(results, __);
+    equal(results, -1);
     start();
-  }, 600);
+  }, 6);
 });
